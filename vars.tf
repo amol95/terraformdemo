d3ce27092d4f13e257f2ed527989c5bfb44f909b
amol95 <60209385+amol95@users.noreply.github.com>
2021-10-20 14:37:18 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
-default = "AKIA2S2N5ZUCU5J7GGBL"
+default = "**********"
 }
 
 variable "aws_secret_key" {
-default = "WZCucoVN0BXM8jtFaG9XHOnycD4NTTOSfhg9aMCX"
+default = "********"
 }
 
 variable "aws_region" {
