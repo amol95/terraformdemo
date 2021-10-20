a050dad8a3426448ac067a49f606e9add774100c
Ubuntu <ubuntu@ip-172-31-25-37.ec2.internal>
2021-10-20 08:11:49 +0000

    3 files added

---
+++ vars.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+default = "AKIA2S2N5ZUCU5J7GGBL"
+}
+
+variable "aws_secret_key" {
+default = "WZCucoVN0BXM8jtFaG9XHOnycD4NTTOSfhg9aMCX"
+}
+
+variable "aws_region" {
+default = "us-east-1"
+}
